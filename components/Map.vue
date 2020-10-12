0ce28a40fe2a1c40fe05adab1ef6309f8436e7d9
TJ <thaddeusjiang@gmail.com>
2020-10-12 23:32:55 +0900

    show map and pin a mark

---
+++ components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="google-map" id="map"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      const location = { lat: 35.626097, lng: 139.782513 };
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: location,
+        zoom: 15
+      });
+      var marker = new google.maps.Marker({
+        position: location,
+        label: "T", // teamLab
+        animation: google.maps.Animation.DROP
+      });
+      marker.setMap(map);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.google-map {
+  width: 100%;
+  height: 400px;
+}
+</style>
